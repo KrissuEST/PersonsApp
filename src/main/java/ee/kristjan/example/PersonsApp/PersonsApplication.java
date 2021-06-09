fa822eedf15572eef3bd1d6d5c664dfb86f80dae
KrissuEST <kristjantonismae@gmail.com>
2021-06-09 15:22:37 +0300

    First commit, adding back-end basic project.

---
+++ src/main/java/ee/kristjan/example/PersonsApp/PersonsApplication.java
@@ -0,0 +1,14 @@
+package ee.kristjan.example.PersonsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* It will be back-end of our application */
+@SpringBootApplication
+public class PersonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/kristjan/example/PersonsApp/PersonsApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.kristjan.example.PersonsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
